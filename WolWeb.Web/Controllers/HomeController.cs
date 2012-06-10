1873bb5dc9771b53b76c08b5d73a99c88a5ea713
Hannes <hannes@sachsenhofer.com>
2012-06-10 18:12:56 +0200

    Added preconfigurable hosts.

--- WolWeb.Web/Controllers/HomeController.cs
+++ WolWeb.Web/Controllers/HomeController.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using WolWeb.ViewModel;
 
 namespace WolWeb.Controllers {
     public class HomeController : Controller {
+
         public ActionResult Index() {
-            return View();
+            var model = new PreconfiguredHostsViewModel(Server.MapPath("~/App_Data/Hosts.txt"));
+            return View(model);
         }
+
     }
 }
---
+++ WolWeb.Web/ViewModel/PreconfiguredHostsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WolWeb.ViewModel {
+    public class PreconfiguredHostsViewModel {
+
+        public PreconfiguredHostsViewModel(string pathToFile) {
+            var hosts = new List<PreconfiguredHost>();
+
+            foreach (var line in File.ReadAllLines(pathToFile).Select(x => x.Trim()).Where(x => !x.StartsWith("#"))) {
+                var parts = line.Split('|');
+                if (parts.Length == 3) {
+                    hosts.Add(new PreconfiguredHost {
+                        Name = parts[0].Trim(),
+                        IpAddress = parts[1].Trim(),
+                        MacAddress = parts[2].Trim()
+                    });
+                }
+            }
+
+            PreconfiguredHosts = hosts;
+        }
+
+        public IEnumerable<PreconfiguredHost> PreconfiguredHosts { get; set; }
+
+        public class PreconfiguredHost {
+            public string Name { get; set; }
+            public string IpAddress { get; set; }
+            public string MacAddress { get; set; }
+        }
+
+    }
+}
