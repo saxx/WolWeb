cda47c14a544067af49dcaf3695e9591ecb1c1a5
Hannes <hannes@sachsenhofer.com>
2012-06-14 22:24:10 +0200

    Cleaned up the code.

--- WolWeb.Web/Controllers/ShutdownController.cs
+++ WolWeb.Web/Controllers/ShutdownController.cs
@@ -1,19 +1,60 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
 using System.Web.Http;
-using Microsoft.Win32.SafeHandles;
 
 namespace WolWeb.Controllers {
     public class ShutdownController : ApiController {
 
         [HttpGet]
         public string Index(string id) {
+
             try {
-                var exitCode = CreateProcessAsUser(@"c:\windows\system32\shutdown.exe", @" /s /t 0 /m " + id);
-                return "ExitCode: " + exitCode;
+                IntPtr dupedToken = new IntPtr(0);
+
+                SECURITY_ATTRIBUTES sa = new SECURITY_ATTRIBUTES();
+                sa.bInheritHandle = false;
+                sa.Length = Marshal.SizeOf(sa);
+                sa.lpSecurityDescriptor = (IntPtr)0;
+
+                var token = WindowsIdentity.GetCurrent().Token;
+
+                const uint GENERIC_ALL = 0x10000000;
+
+                const int SecurityImpersonation = 2;
+                const int TokenType = 1;
+
+                var ret = DuplicateTokenEx(token, GENERIC_ALL, ref sa, SecurityImpersonation, TokenType, ref dupedToken);
+                if (ret == false)
+                    throw new Exception("DuplicateTokenEx failed with " + Marshal.GetLastWin32Error());
+
+                STARTUPINFO si = new STARTUPINFO();
+                si.cb = Marshal.SizeOf(si);
+                si.lpDesktop = "";
+                PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+
+                uint exitCode;
+                try {
+                    ret = CreateProcessAsUser(dupedToken, null, @"c:\windows\system32\shutdown.exe /s /t 0 /m " + id, ref sa, ref sa, false, 0, (IntPtr)0, "c:\\", ref si, out pi);
+
+                    if (ret == false)
+                        throw new Exception("CreateProcessAsUser failed with " + Marshal.GetLastWin32Error());
+
+                    WaitForSingleObject(pi.hProcess, 10000);
+                    GetExitCodeProcess(pi.hProcess, out exitCode);
+                }
+                catch (Exception ex) {
+                    throw ex;
+                }
+                finally {
+                    CloseHandle(pi.hProcess);
+                    CloseHandle(pi.hThread);
+                    CloseHandle(dupedToken);
+                }
+
+                if (exitCode == 0)
+                    return "Success";
+                return "Failed. Exitcode: " + exitCode;
             }
             catch (Exception ex) {
                 return ex.Message;
@@ -21,136 +62,64 @@ namespace WolWeb.Controllers {
         }
 
 
-        //took most of the code from http://odetocode.com/Blogs/scott/archive/2004/10/29/createprocessasuser.aspx
-        private uint CreateProcessAsUser(string executeable, string arguments) {
-            IntPtr hToken = WindowsIdentity.GetCurrent().Token;
-            IntPtr hDupedToken = IntPtr.Zero;
-
-
-            ProcessUtility.PROCESS_INFORMATION pi = new ProcessUtility.PROCESS_INFORMATION();
-
-            try {
-                ProcessUtility.SECURITY_ATTRIBUTES sa = new ProcessUtility.SECURITY_ATTRIBUTES();
-                sa.Length = Marshal.SizeOf(sa);
-
-                bool result = ProcessUtility.DuplicateTokenEx(hToken, ProcessUtility.GENERIC_ALL_ACCESS, ref sa, (int)ProcessUtility.SECURITY_IMPERSONATION_LEVEL.SecurityIdentification, (int)ProcessUtility.TOKEN_TYPE.TokenPrimary, ref hDupedToken);
-
-                if (!result)
-                    throw new ApplicationException("DuplicateTokenEx failed");
-
-                ProcessUtility.STARTUPINFO si = new ProcessUtility.STARTUPINFO();
-                si.cb = Marshal.SizeOf(si);
-                si.lpDesktop = String.Empty;
-
-
-                result = ProcessUtility.CreateProcessAsUser(hDupedToken, executeable, arguments, ref sa, ref sa, false, 0, IntPtr.Zero, @"C:\", ref si, ref pi);
-
-
-                if (!result) {
-                    var error = Marshal.GetLastWin32Error();
-                    var message = String.Format("Error: {0}", error);
-                    throw new ApplicationException(message);
-                }
-
-                // Successfully created the process. Wait for it to finish.
-                ProcessUtility.WaitForSingleObject(pi.hProcess, 10000);
-
-                // Get the exit code.
-                uint exitCode;
-                ProcessUtility.GetExitCodeProcess(pi.hProcess, out exitCode);
-
-                return exitCode;
-            }
-            finally {
-                if (pi.hProcess != IntPtr.Zero)
-                    ProcessUtility.CloseHandle(pi.hProcess);
-                if (pi.hThread != IntPtr.Zero)
-                    ProcessUtility.CloseHandle(pi.hThread);
-                if (hDupedToken != IntPtr.Zero)
-                    ProcessUtility.CloseHandle(hDupedToken);
-            }
+        //took most of the code from http://support.microsoft.com/kb/889251/en-us?fr=1
+        [StructLayout(LayoutKind.Sequential)]
+        public struct STARTUPINFO {
+            public int cb;
+            public String lpReserved;
+            public String lpDesktop;
+            public String lpTitle;
+            public uint dwX;
+            public uint dwY;
+            public uint dwXSize;
+            public uint dwYSize;
+            public uint dwXCountChars;
+            public uint dwYCountChars;
+            public uint dwFillAttribute;
+            public uint dwFlags;
+            public short wShowWindow;
+            public short cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
         }
 
-
-
-
-        public class ProcessUtility {
-            [StructLayout(LayoutKind.Sequential)]
-            public struct STARTUPINFO {
-                public Int32 cb;
-                public string lpReserved;
-                public string lpDesktop;
-                public string lpTitle;
-                public Int32 dwX;
-                public Int32 dwY;
-                public Int32 dwXSize;
-                public Int32 dwXCountChars;
-                public Int32 dwYCountChars;
-                public Int32 dwFillAttribute;
-                public Int32 dwFlags;
-                public Int16 wShowWindow;
-                public Int16 cbReserved2;
-                public IntPtr lpReserved2;
-                public IntPtr hStdInput;
-                public IntPtr hStdOutput;
-                public IntPtr hStdError;
-            }
-
-            [StructLayout(LayoutKind.Sequential)]
-            public struct PROCESS_INFORMATION {
-                public IntPtr hProcess;
-                public IntPtr hThread;
-                public Int32 dwProcessID;
-                public Int32 dwThreadID;
-            }
-
-            [StructLayout(LayoutKind.Sequential)]
-            public struct SECURITY_ATTRIBUTES {
-                public Int32 Length;
-                public IntPtr lpSecurityDescriptor;
-                public bool bInheritHandle;
-            }
-
-            public enum SECURITY_IMPERSONATION_LEVEL {
-                SecurityAnonymous,
-                SecurityIdentification,
-                SecurityImpersonation,
-                SecurityDelegation
-            }
-
-            public enum TOKEN_TYPE {
-                TokenPrimary = 1,
-                TokenImpersonation
-            }
-
-            public const int GENERIC_ALL_ACCESS = 0x10000000;
-
-            [DllImport("kernel32.dll", EntryPoint = "CloseHandle", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-            public static extern bool CloseHandle(IntPtr handle);
-
-            [DllImport("advapi32.dll", EntryPoint = "CreateProcessAsUser", SetLastError = true, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
-            public static extern bool
-               CreateProcessAsUser(IntPtr hToken, string lpApplicationName, string lpCommandLine,
-                                   ref SECURITY_ATTRIBUTES lpProcessAttributes, ref SECURITY_ATTRIBUTES lpThreadAttributes,
-                                   bool bInheritHandle, Int32 dwCreationFlags, IntPtr lpEnvrionment,
-                                   string lpCurrentDirectory, ref STARTUPINFO lpStartupInfo,
-                                   ref PROCESS_INFORMATION lpProcessInformation);
-
-            [DllImport("advapi32.dll", EntryPoint = "DuplicateTokenEx")]
-            public static extern bool DuplicateTokenEx(IntPtr hExistingToken, Int32 dwDesiredAccess,
-                                ref SECURITY_ATTRIBUTES lpThreadAttributes,
-                                Int32 ImpersonationLevel, Int32 dwTokenType,
-                                ref IntPtr phNewToken);
-
-
-
-[DllImport("kernel32.dll", SetLastError = true)]
-[return: MarshalAs(UnmanagedType.Bool)]
-public static extern bool GetExitCodeProcess(IntPtr hProcess, out uint lpExitCode);
-
-            [DllImport("kernel32.dll", SetLastError=true)]
-  public static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public uint dwProcessId;
+            public uint dwThreadId;
         }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES {
+            public int Length;
+            public IntPtr lpSecurityDescriptor;
+            public bool bInheritHandle;
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "CloseHandle", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public extern static bool CloseHandle(IntPtr handle);
+
+        [DllImport("advapi32.dll", EntryPoint = "CreateProcessAsUser", SetLastError = true, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public extern static bool CreateProcessAsUser(IntPtr hToken, String lpApplicationName, String lpCommandLine, ref SECURITY_ATTRIBUTES lpProcessAttributes,
+            ref SECURITY_ATTRIBUTES lpThreadAttributes, bool bInheritHandle, int dwCreationFlags, IntPtr lpEnvironment,
+            String lpCurrentDirectory, ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("advapi32.dll", EntryPoint = "DuplicateTokenEx")]
+        public extern static bool DuplicateTokenEx(IntPtr ExistingTokenHandle, uint dwDesiredAccess,
+            ref SECURITY_ATTRIBUTES lpThreadAttributes, int TokenType,
+            int ImpersonationLevel, ref IntPtr DuplicateTokenHandle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetExitCodeProcess(IntPtr hProcess, out uint lpExitCode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+
 
 
     }
