a04ebae2f48485c1880bc93e044ea4f230e2e90c
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2012-07-15 09:22:05 +0200

    Added AppFail.

--- WolWeb.Web/App_Start/FilterConfig.cs
+++ WolWeb.Web/App_Start/FilterConfig.cs
@@ -1,9 +1,11 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using AppfailReporting.Mvc;
 
 namespace WolWeb {
     public class FilterConfig {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new AppfailReportAttribute()); 
             filters.Add(new HandleErrorAttribute());
         }
     }
